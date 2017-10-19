ea4b7433da55351b71ec5bc67a54c217b359bd1c
Jay Garcia <jay.g@portalink.com>
2017-10-19 12:48:26 +0800

    Formatting changes + trying out repeating backgrounds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'The Story Circle';
   slogan = 'Living the Magic, Sharing the Smiles';
+  year = new Date().getFullYear().toString();
   isNavbarCollapsed = true;
 }
