1ae1e0d95505155c985c0469a3c7186573729972
Jay Garcia <jay.g@portalink.com>
2017-10-19 13:19:00 +0800

    Minor edits + changes to serving port (Openshift)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
 
+// Components
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-
-import { AppRoutingModule } from './app-routing.module';
-
-import { MemberService } from './members/member.service';
 import { HomepageComponent } from './homepage/homepage.component';
 import { EventsComponent } from './events/events.component';
+
+// Router
+import { AppRoutingModule } from './app-routing.module';
+
+// Services
+import { MemberService } from './members/member.service';
+
 
 @NgModule({
   declarations: [
