30566ed65c0a6af4438c860342a7ef16f9dabf59
Jayman X <jaymanx4life@gmail.com>
2018-03-22 20:13:24 +0800

    Initial re-design of front-end

--- src/assets/js/creative.js
+++ src/assets/js/creative.js
@@ -33,11 +33,3 @@
     }, 300);
 
 })(jQuery); // End of use strict
-
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=368926963549934';
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
